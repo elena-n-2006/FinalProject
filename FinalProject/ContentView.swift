59198be26ef66fb2e43884105e44f1bb385be6f0
Sydney Ramos <sydneyramos2028@gmail.com>
2025-07-15 13:23:45 -0400

    chnaged background color to a light pink

--- FinalProject/ContentView.swift
+++ FinalProject/ContentView.swift
@@ -11,20 +11,22 @@ struct ContentView: View {
     var body: some View {
         NavigationStack{
             ZStack {
+                Color(red: 0.95, green: 0.75, blue: 0.85)                .ignoresSafeArea()
+                
                 HStack{
                     
                     VStack {
                         Text("Make a blog")
                         Text("Fun Fact of the Day:")
                         Text("Here's your emoji chart of the week")
-                    }
+                                    }
                 }
             }
-            .padding()
+            
+            
         }
     }
 }
-
 #Preview {
     ContentView()
 }
