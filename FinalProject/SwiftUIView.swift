447936ef0caac99c860a4d369ef2e027a7df9964
Gianna <gianna.s.zhang@gmail.com>
2025-07-16 11:11:48 -0400

    button for fun fact

--- FinalProject/SwiftUIView.swift
+++ FinalProject/SwiftUIView.swift
@@ -5,14 +5,44 @@
 //  Created by Gianna on 7/16/25.
 //
 
+
 import SwiftUI
 
-struct SwiftUIView: View {
+struct BrainFact: View {
+    @State private var goToFacts = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            ZStack {
+                HStack{
+                    
+                    VStack {
+                        Text("Make a blog")
+                        Text("Fun Fact of the Day:")
+                        Text("Here's your emoji chart of the week")
+                    }
+                }
+            
+            }
+            Button(action: {
+                                    goToFacts = true
+            }) {
+                Text("Show Me a Brain Fact")
+                    .font(.headline)
+                    .padding()
+                    .frame(width: 240)
+                    .background(Color.pink)
+                    .foregroundColor(.white)
+                .cornerRadius(12)}
+         
+            NavigationLink(destination: BrainFact()){
+                Text("Show Brain Fact")
+            }
+                                    
+            .padding()
+        }
     }
 }
 
 #Preview {
-    SwiftUIView()
+    ContentView()
 }
