8ee3a57daa9108502169eec06a778165a960640c
Elena Nguyen <elenabackupnumber3@gmail.com>
2025-07-17 10:25:45 -0400

    changed the name of the brain facts file and also added some more UI things to the blog part

--- FinalProject/BlogPostView.swift
+++ FinalProject/BlogPostView.swift
@@ -10,16 +10,25 @@ import SwiftData
 
 struct BlogPostView: View {
     var body: some View {
-        Text("What's your mood today?")
-            .font(.title)
-            .padding()
+        VStack{
+            Text("What's your mood today?")
+                .font(.title)
+                .padding()
+            Menu("Emojis:") {
+                Text("Test")
+                Text("Test")
+                Text("test")
+            }
+            TextEditor(text: /*@START_MENU_TOKEN@*/.constant("Placeholder")/*@END_MENU_TOKEN@*/)
+                .frame(height: 300)
+                .padding()
+        }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .background(Color.gray.opacity(0.2))
             .cornerRadius(20)
             .padding()
             .foregroundColor(.white)
             .shadow(radius: 10)
-        
     }
 }
 
